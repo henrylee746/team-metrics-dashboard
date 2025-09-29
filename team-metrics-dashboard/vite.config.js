c9184f76cdc80a92df68b534e7e6228d654cabea
henrylee746 <henry.lee746@gmail.com>
2025-09-28 23:21:46 -0400

    TailwindCSS import changes

--- team-metrics-dashboard/vite.config.js
+++ team-metrics-dashboard/vite.config.js
@@ -2,10 +2,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc"; // Use SWC instead of Babel
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
